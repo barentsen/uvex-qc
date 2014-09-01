ff89617985d27a1a1a692d6f5cc2f9f9d4525a0b
Geert Barentsen <geert@barentsen.be>
2014-09-01 12:25:02 +0100

    initial commit

---
+++ data/observing/update-fields-observed.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Creates a list of unique fields observed recently
+grep uvex seeing-logs/seeing_* | awk '{ print $10 }' | sed s/'uvex_'/''/g | sort | uniq > fields-observed-in-2014.txt 
