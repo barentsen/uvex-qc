dac7f2836e9e2abfd8852af7de04dc6e9981023a
Geert Barentsen <geert@barentsen.be>
2015-09-20 21:35:55 -0700

    also check if conf map exists

--- data/image-index/test_json_files.py
+++ data/image-index/test_json_files.py
@@ -8,7 +8,9 @@ from astropy import log
 
 
 RUN2PATH = json.load(open("run2path.json", "r"))
-COLSTOCHECK = ["runno_u", "runno_g", "runno_r"]
+DIR2CONF = json.load(open("dir2conf.json", "r"))
+
+BANDS = ["u", "g", "r"]
 
 if __name__ == "__main__":
     qc = Table.read("/home/gb/dev/uvex-qc/data/casu-dqc/uvex-casu-dqc-by-field.fits")
@@ -17,7 +19,15 @@ if __name__ == "__main__":
         if row["dir"] in ["jul2013", "dec2011", "nov2012", "dec2012", "dec2012b"]:
             continue
 
-        if np.all([row[col] != "" for col in COLSTOCHECK]):
-            for col in COLSTOCHECK:
-                if not row[col] in RUN2PATH:
-                    log.error("{}".format(row[col]))
+        if np.all([row["runno_"+band] != "" for band in BANDS]):
+            for band in ["u", "g", "r"]:
+                runno = row["runno_"+band]
+
+                # Check if we have the image FITS file for the run number
+                if not runno in RUN2PATH:
+                    log.error("{}".format(runno))
+
+                    # Check if we know the confidence map for this directory
+                    directory = os.path.dirname(RUN2PATH[runno])
+                    if directory not in DIR2CONF or band not in DIR2CONF[directory]:
+                        log.error("No conf for {}".format(directory))
