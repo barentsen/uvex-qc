a88dc7c2f63095cafaafef31f1099f4d74b499b8
Geert Barentsen <geert@barentsen.be>
2015-09-23 19:49:41 +0100

    Quicklook cluster scripts

--- quicklook/print-cluster-jobs.py
+++ quicklook/print-cluster-jobs.py
@@ -5,6 +5,6 @@ from astropy.table import Table
 if __name__ == "__main__":
     t = Table.read('/home/gb/dev/uvex-qc/data/casu-dqc/uvex-casu-dqc-by-field.fits')
     for dirname in np.unique(t["dir"]):
-        cmd = 'qsub -v DIRNAME={} -N uvex_ql_{} quicklook.pbs'.format(
+        cmd = 'qsub -v DIRNAME={} -N iphas_ql_{} quicklook.pbs'.format(
                     dirname, dirname)
         print(cmd)
